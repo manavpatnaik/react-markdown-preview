f28a1c2b71d641581dc9b52241d1a67e274d1f87
Manav Patnaik <manavpatnaik@hotmail.com>
2021-03-25 16:13:12 +0530

    Finished app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CodeIcon from "@material-ui/icons/Code";
+import { Container, Grid, TextField, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import ReactMarkdown from "react-markdown";
 
-function App() {
+const useStyles = makeStyles({
+  header: {
+    marginTop: 20,
+    marginBottom: 30,
+  },
+  body: {
+    margin: 0,
+    boxSizing: "border-box",
+  },
+  field: {
+    backgroundColor: "white",
+  },
+  icon: {
+    fontSize: 60,
+    marginBottom: -15,
+    marginRight: 10,
+  },
+});
+
+const App = () => {
+  const classes = useStyles();
+  const [markdown, setMarkdown] = React.useState("## Hello World");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={classes.body}>
+      <Container>
+        <Typography
+          variant="h3"
+          align="center"
+          className={classes.header}
+          gutterBottom
         >
-          Learn React
-        </a>
-      </header>
+          <CodeIcon className={classes.icon} />
+          Markdown Preview
+        </Typography>
+        <Grid container spacing={6}>
+          <Grid item sm={12} md={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={markdown}
+              className={classes.field}
+              multiline
+              rows={25}
+              onChange={(e) => setMarkdown(e.target.value)}
+            />
+          </Grid>
+          <Grid item sm={12} md={6}>
+            <Typography>
+              <ReactMarkdown>{markdown}</ReactMarkdown>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
